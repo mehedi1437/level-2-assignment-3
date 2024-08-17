71500fb3d7e9a234c4fe4b060be2501f33573c97
mehediemon1437 <mdmehedihasanemon1437@gmail.com>
2024-08-17 09:53:33 +0600

    dist folder deleted

--- dist/app/modules/slot/slot.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.slotRoutes = void 0;
-const express_1 = require("express");
-const slot_controller_1 = require("./slot.controller");
-const router = (0, express_1.Router)();
-router.get("/availability", slot_controller_1.slotController.getAvaliableSlots);
-exports.slotRoutes = router;
