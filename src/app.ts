579e4e93de8969c8b599af00ea9596da4744bad2
mehediemon1437 <mdmehedihasanemon1437@gmail.com>
2024-08-17 12:16:04 +0600

    final commit

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(cors())
 app.use('/api/', router)
 
  app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
+  res.send('Car Washing running !')
 })
 app.use(globalErrorHandler);
 
